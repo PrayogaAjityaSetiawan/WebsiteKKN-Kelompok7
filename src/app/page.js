8d7a6e7c3866080a24fe2f60317dc929cc860a4d
PrayogaAjityaSetiawan <ajityaprayoga123@gmail.com>
2025-09-02 18:27:00 +0700

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,132 @@
-import Image from "next/image";
+"use client";
+import { FocusCards } from "@/components/ui/focus-cards";
+import { InfiniteMovingCards } from "@/components/ui/infinite-moving-cards";
+import { TextScroll } from "@/components/ui/text-scroll";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="min-h-screen w-full bg-[#fefcff] relative px-4 md:px-20">
+  <div
+    className="absolute inset-0 z-0"
+    style={{
+      backgroundImage: `
+        linear-gradient(to right, rgba(229,231,235,0.8) 1px, transparent 1px),
+        linear-gradient(to bottom, rgba(229,231,235,0.8) 1px, transparent 1px),
+        radial-gradient(circle 500px at 0% 20%, rgba(139,92,246,0.3), transparent),
+        radial-gradient(circle 500px at 100% 0%, rgba(59,130,246,0.3), transparent)
+      `,
+      backgroundSize: "20px 20px,20px 20px, 100% 100%, 100% 100%",
+    }}
+  />
+  <div className=" h-[100vh] flex flex-col gap-5 justify-center md:jus md:flex-row items-center  md:pt-0">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="md:w-1/2 flex flex-col justify-center gap-2 items-center z-10">
+      <h1 className="text-[40px] md:text-[60px] leading-none font-semibold uppercase">Keluarga Baru, Cerita Tak Terlupakan</h1>
+      <p className="text-gray-500">KKN Kelompok 7 bukan hanya tentang pengabdian, tapi juga tentang kebersamaan, tawa, dan kisah yang mengikat hati kami sebagai sebuah keluarga.</p>
     </div>
+    <div className="md:w-1/2 z-20"> 
+        <img className="w-full h-full object-cover md:rounded-lg" src="./bg1.jpg" alt=""/>
+    </div>
+  </div>
+  <div>
+
+  <TextScroll 
+  className="font-display text-center text-4xl font-semibold tracking-tighter  text-black dark:text-white md:text-7xl md:leading-[5rem]"
+  text="KKN Sumberjaya 2025" 
+  default_velocity={5}/>
+  </div>
+  <div className="my-10 md:mx-20">
+    <div className="flex flex-col gap-2 md:flex-row justify-between md:items-center mb-5">
+      <div className="flex flex-col  gap-2 md:w-1/2 z-10">
+        <span className="text-gray-500 font-semibold">Our Family</span>
+        <h1 className="text-[35px] md:text-[40px] leading-none font-bold">Together as One Heart</h1>
+      </div>
+      <div className="md:w-1/2 z-10">
+        <span className="text-gray-500 font-semibold">Kebersamaan kami bukan hanya dalam kegiatan, tapi juga dalam ikatan kekeluargaan yang tumbuh dari setiap cerita dan pengalaman. </span>
+      </div>
+    </div>
+
+    <FocusCards className
+    cards={[
+      {title: "Rahmad Maulana", src: "./rahmad.jpg"},
+      {title: "Prayoga Ajitya Setiawan", src: "./aku.jpg"},
+      {title: "Dzaky Rakha Meilano", src: "./dzaky.jpg"},
+      {title: "Sabila Zulfa", src: "./sabila.jpg"},
+      {title: "Syifa Salsabila Farmin", src: "./syifa.jpg"},
+      {title: "Danu Dyeka Putra", src: "./danu.jpg"},
+      {title: "Reza Andira", src: "./reza.jpg"},
+      {title: "Anneira Audrey", src: "./audrey.jpg"},
+      {title: "Dhias Prastowo", src: "./dias.jpg"},
+      {title: "Alista Nur Aprilia", src: "./alista.jpg"},
+    ]} />
+
+    <div className="h-[40rem] rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
+    <div className="flex flex-col gap-2 md:gap-0 md:flex-row justify-between md:items-center mb-5">
+      <div className="flex flex-col md:gap-2 md:w-1/2 z-10">
+        <span className="text-gray-500 font-semibold">Suara Hati</span>
+        <h1 className="text-[35px] md:text-[40px] leading-none font-bold">Pesan dan Kesan ❤️</h1>
+      </div>
+      <div className="md:w-1/2 z-10">
+        <span className="text-gray-500 font-semibold">Curahan kata dari hati kita, yang selalu rindu soal kenangan dan kebersamann yang telah kita buat</span>
+      </div>
+    </div>
+      <InfiniteMovingCards
+        items={testimonials}
+        direction="right"
+        speed="slow"
+      />
+    </div>
+
+  </div>
+</div>
+    
   );
 }
+
+
+const testimonials = [
+  {
+    quote:
+      "kesan dan pesan saya KKN di Pandeglang memberi pengalaman berharga tentang kebersamaan dan kepedulian.bersyukur satu posko dengan teman-teman yang kompak dan luar biasa.setiap kegiatan menjadi kenangan indah tak terlupakan.saya belajar arti gotong royong dan adaptasi di lingkungan baru.terima kasih kepada warga Desa Sumber Jaya atas sambutan hangatnya.",
+    name: "Syifa Salsabila Farmin",
+    title: "Konsumi",
+  },
+  {
+    quote:
+      "Grup kita memang paling pecah! Kesan saya selama ini, seru banget bisa kerja, belajar bareng kalian. Setiap anggota memiliki keunikan dan kontribusi baik dari segi ide maupun semangat pantang menyerah. Sampai jumpa di lain kesempatan. Semoga pertemanan ini ga putus di sini ya. Jangan sampai putus komunikasi ya! Mari kita jaga silaturahmi ini di luar urusan akademis.",
+    name: "Rahmad Maulana",
+    title: "Ketua",
+  },
+  {
+    quote:
+      "aneh aneh aneh",
+    name: "Alista Nur Aprilia",
+    title: "PDD",
+  },
+  {
+    quote:
+      "Haiii, tengkyu ya semua atas 24 hari kebersamaannya. Banyak moment dan pengalaman baru yang gw dapetin selama kkn, pasti ga akan gw lupain. Senang sekali bisa kenal kalian semua, sehat dan sukses terus ya buat kedepannya. Semoga silaturahmi kita tidak terputus, terima kasih orang-orang baik🫶",
+    name: "Sabila Zulfa",
+    title: "Acara",
+  },
+  {
+    quote:
+      "Makasih buat 3 minggu nya, kasihan mamad terbuli #JusticeForMamad",
+    name: "Anneira Audrey",
+    title: "Bendahara",
+  },
+  {
+    quote:
+      "Sebenernya gada kesan pesan dri gua",
+    name: "Reza Andira",
+    title: "Humas",
+  },
+  {
+    quote:
+      "Terima kasih untuk kalian semua, senang sekali bisa kenal kalian semua, semoga kita selalu ingat ya 🙏",
+    name: "Prayoga Ajitya Setiawan",
+    title: "Sekretaris",
+  },
+  
+];
+
